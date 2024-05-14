49b6b07fcb014f752cdee308d486c5e5eabcfc24
DMichelR <aa6132788@gmail.com>
2024-05-13 23:44:52 -0400

    add port and path handler

--- main.c
+++ main.c
@@ -11,106 +11,105 @@
 #define MAX_CONNECTIONS 30
 
 const char *server_info = "Server: MyHTTPServer/1.0\r\n\r\n";
-const char *BASE_PATH = "/home/fundacion/CLionProjects/Unix-Final/";
+int server_fd;
 
+char *get_path() {
+    char *path = malloc(BUFFER_SIZE);
+
+    if (getcwd(path, BUFFER_SIZE) == NULL) {
+        perror("Failed to get current working directory.");
+        return NULL;
+    }
+
+    strcat(path, "/");
+
+    return path;
+}
+
+void handle_sigint(int sig) {
+    printf("\nServer stopped.\n");
+    close(server_fd);
+    exit(EXIT_SUCCESS);
+}
+
+void send_response(int client_socket, const char *status, const char *content_type, const char *message) {
+    char response[BUFFER_SIZE];
+    sprintf(response, "HTTP/1.0 %s\r\n%sContent-Type: %s\r\n\r\n%s", status, server_info, content_type, message);
+    send(client_socket, response, strlen(response), 0);
+}
+
+void handle_file_request(int client_socket, const char *filename, const char *content_type) {
+    char full_path[BUFFER_SIZE];
+    char *path = get_path();
+    sprintf(full_path, "%s%s", path, filename);
+    printf("Opening file: %s\n", full_path);
+
+    FILE *file = fopen(full_path, "r");
+    if (file != NULL) {
+        send_response(client_socket, "200 OK", content_type, "");
+        char buffer[BUFFER_SIZE];
+        while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+            send(client_socket, buffer, strlen(buffer), 0);
+        }
+        fclose(file);
+    } else {
+        send_response(client_socket, "404 Not Found", "text/plain", "File not found.");
+    }
+}
+
+void handle_python_request(int client_socket, const char *filename) {
+    char full_path[BUFFER_SIZE];
+    char *path = get_path();
+    sprintf(full_path, "%s%s", path, filename);
+    printf("Opening file: %s\n", full_path);
+
+    FILE *file = fopen(full_path, "r");
+    if (file != NULL) {
+        FILE *python_output;
+        char python_command[BUFFER_SIZE];
+        char python_buffer[BUFFER_SIZE];
+
+        sprintf(python_command, "python3 %s", full_path);
+        python_output = popen(python_command, "r");
+        if (python_output != NULL) {
+            send_response(client_socket, "200 OK", "text/plain", "");
+            while (fgets(python_buffer, BUFFER_SIZE, python_output) != NULL) {
+                send(client_socket, python_buffer, strlen(python_buffer), 0);
+            }
+            pclose(python_output);
+        } else {
+            send_response(client_socket, "500 Internal Server Error", "text/py", "Error executing Python script.");
+        }
+        fclose(file);
+    } else {
+        send_response(client_socket, "404 Not Found", "text/plain", "File not found.");
+    }
+}
 
 void handle_connection(int client_socket) {
-    char buffer[BUFFER_SIZE] = {0};
-    char response[BUFFER_SIZE] = {0};
-
-    // Read the HTTP request from the client
+    char buffer[BUFFER_SIZE];
     read(client_socket, buffer, BUFFER_SIZE);
 
-    // Parse the HTTP request to check for GET or POST and the requested path
     char method[5];
     char path[BUFFER_SIZE];
     sscanf(buffer, "%s %s", method, path);
 
-    if  (strcmp(method, "GET") == 0) {
-        // Extract the filename from the path
-        char *filename;
+    if (strcmp(method, "GET") == 0) {
         if (strcmp(path, "/") == 0) {
-            // Send the server info as response for "/"
-            sprintf(response, "HTTP/1.0 200 OK\r\n%sContent-Type: text/plain\r\n%s", server_info, server_info);
-            send(client_socket, response, strlen(response), 0);
-            return;
+            send_response(client_socket, "200 OK", "text/plain", server_info);
         } else {
-            filename = strrchr(path, '/') + 1;
-
-            // Open the requested file
-            char full_path[BUFFER_SIZE];
-            sprintf(full_path, "%s%s", BASE_PATH, filename);
-            printf("Opening file: %s\n", full_path);
-
-            FILE *file = fopen(full_path, "r");
-            if (file != NULL) {
-                // File found, send its content in the response
-                char header[BUFFER_SIZE];
-                sprintf(header, "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n");
-                send(client_socket, header, strlen(header), 0);
-                while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-                    send(client_socket, buffer, strlen(buffer), 0);
-                }
-                fclose(file);
-            } else {
-                // File not found
-                sprintf(response, "HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\n\r\nFile not found.");
-                send(client_socket, response, strlen(response), 0);
-            }
+            char *filename = strrchr(path, '/') + 1;
+            handle_file_request(client_socket, filename, "text/html");
         }
-    } else if (strcmp(method, "POST") == 0 && strstr(path, ".py") != NULL) {
-        // Handle POST request to a specific Python file
-        // Extract the filename from the path
-        char *filename = strrchr(path, '/') + 1;
-
-        char full_path[BUFFER_SIZE];
-        sprintf(full_path, "%s%s", BASE_PATH, filename);
-        printf("Opening file: %s\n", full_path);
-        // Open the requested Python file
-        FILE *file = fopen(full_path, "r");
-
-        if (file != NULL) {
-            // File found, execute it with Python interpreter
-            FILE *python_output;
-            char python_command[BUFFER_SIZE];
-            char python_buffer[BUFFER_SIZE];
-            char response[BUFFER_SIZE];
-
-            // Build the command to execute the Python script
-            sprintf(python_command, "python3 %s%s", BASE_PATH, filename);
-
-            // Open a pipe to read the output of the Python script
-            python_output = popen(python_command, "r");
-            if (python_output != NULL) {
-                // Send HTTP response header
-                sprintf(response, "HTTP/1.0 200 OK\r\n%sContent-Type: text/plain\r\n\r\n", server_info);
-                send(client_socket, response, strlen(response), 0);
-
-                // Read and send the output of the Python script line by line
-                while (fgets(python_buffer, BUFFER_SIZE, python_output) != NULL) {
-                    send(client_socket, python_buffer, strlen(python_buffer), 0);
-                }
-
-                // Close the pipe
-                pclose(python_output);
-            } else {
-                // Error executing the Python script
-                sprintf(response, "HTTP/1.0 500 Internal Server Error\r\n%sContent-Type: text/plain\r\n\r\nError executing Python script.", server_info);
-                send(client_socket, response, strlen(response), 0);
-            }
-
-            // Close the file
-            fclose(file);
+    } else if (strcmp(method, "POST") == 0) {
+        if (strstr(path, ".py") != NULL) {
+            char *filename = strrchr(path, '/') + 1;
+            handle_python_request(client_socket, filename);
         } else {
-            // File not found, return 404 Not Found
-            sprintf(response, "HTTP/1.0 404 Not Found\r\n%sContent-Type: text/plain\r\n\r\nFile not found.", server_info);
-            send(client_socket, response, strlen(response), 0);
+            send_response(client_socket, "400 Bad Request", "text/plain", "Bad Request.");
         }
-    }
-    else {
-        // Invalid request method or path
-        sprintf(response, "HTTP/1.0 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nBad Request.");
-        send(client_socket, response, strlen(response), 0);
+    } else {
+        send_response(client_socket, "400 Bad Request", "text/plain", "Bad Request.");
     }
 
     close(client_socket);
@@ -122,13 +121,11 @@ int main() {
     int opt = 1;
     int addrlen = sizeof(address);
 
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                    &opt, sizeof(opt))) {
         perror("setsockopt");
@@ -136,11 +133,10 @@ int main() {
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 8080
     if (bind(server_fd, (struct sockaddr *)&address,
-             sizeof(address))<0) {
+             sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -149,25 +145,26 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    signal(SIGINT, handle_sigint);
+
     while (1) {
-        printf("\nWaiting for a connection...\n\n");
+        printf("\nWaiting...\n\n");
         if ((client_socket = accept(server_fd, (struct sockaddr *)&address,
-                                    (socklen_t*)&addrlen))<0) {
+                                    (socklen_t *)&addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
 
-        // Fork a new process to handle the connection
         pid_t pid = fork();
-        if (pid == 0) { // Child process
-            close(server_fd); // Close the listening socket in the child process
+        if (pid == 0) {
+            close(server_fd);
             handle_connection(client_socket);
-            exit(EXIT_SUCCESS); // Exit the child process after handling the connection
-        } else if (pid < 0) { // Fork failed
+            exit(EXIT_SUCCESS);
+        } else if (pid < 0) {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else { // Parent process
-            close(client_socket); // Close the client socket in the parent process
+        } else {
+            close(client_socket);
         }
     }
     return 0;
